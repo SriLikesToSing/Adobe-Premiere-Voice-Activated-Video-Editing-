a507d9ac631f86facef65692218dd765b928a0bc
Sri <48895259+SriLikesToSing@users.noreply.github.com>
2022-11-02 19:11:44 -0700

    Update Main.py

--- Voice_Video_editor/Main.py
+++ Voice_Video_editor/Main.py
@@ -1,28 +1,5 @@
 #!/usr/bin/env python
 
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""Google Cloud Speech API sample application using the streaming API.
-NOTE: This module requires the dependencies `pyaudio` and `termcolor`.
-To install using pip:
-    pip install pyaudio
-    pip install termcolor
-Example usage:
-    python transcribe_streaming_infinite.py
-"""
-
 # [START speech_transcribe_infinite_streaming]
 
 import time
